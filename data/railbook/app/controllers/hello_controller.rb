3213b44ef245589143d322965aa45552bf6b30db
yusk <tdfagamdb186@yahoo.co.jp>
2015-06-04 15:25:46 +0900

    初期ファイルの作成(3章まで完了済み)

---
+++ data/railbook/app/controllers/hello_controller.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+class HelloController < ApplicationController
+
+	def index
+		render text: 'こんにちは、世界！'
+	end
+
+	def view
+		@msg = 'hello,world！'
+	end
+
+	def list
+		@books = Book.all
+	end
+
+	def app_var
+		render text: MY_APP['logo']['source']
+	end
+
+	def test1
+		
+	end
+end
